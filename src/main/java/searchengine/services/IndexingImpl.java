c2f1c2d8ade7a5e982e2d004dc56ee72ac8f433b
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-08-26 01:34:01 +0500

    реализовал класс

--- src/main/java/searchengine/services/IndexingImpl.java
+++ src/main/java/searchengine/services/IndexingImpl.java
@@ -1,25 +1,69 @@
 package searchengine.services;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import searchengine.Busines.LinkHandling.LinkParser;
-import searchengine.services.Interface.Indexing;
+import searchengine.config.SitesList;
+import searchengine.dto.FailedResult;
+import searchengine.dto.Result;
+import searchengine.repositories.IndexRepository;
+import searchengine.repositories.LemmaRepository;
+import searchengine.repositories.PageRepository;
+import searchengine.repositories.SiteRepository;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 
+@Service
 @RequiredArgsConstructor
 public class IndexingImpl implements Indexing {
-    String url;
-    public static AtomicBoolean atomicBoolean;
+    private final SitesList sitesList;
+    public static AtomicBoolean atomicBoolean = new AtomicBoolean(false);
+    @Autowired
+    private final SiteRepository siteRepository;
+    @Autowired
+    private final PageRepository pageRepository;
+    @Autowired
+    private final LemmaRepository lemmaRepository;
+    @Autowired
+    private final IndexRepository indexRepository;
 
-    LinkParser linkParser = new LinkParser(url);
     @Override
     public ResponseEntity<?> stop() {
-        return linkParser.stopParse();
+        ResponseEntity<?> responseEntity;
+        Result result = new Result();
+        result.setResult(atomicBoolean.get());
+        if (result.isResult()){
+            responseEntity = ResponseEntity.status(HttpStatus.OK).body(true);
+            atomicBoolean.getAndSet(false);
+        }
+        else {
+            FailedResult failedResult = new FailedResult();
+            failedResult.setResult(result);
+            failedResult.setError("Indexing not started");
+            responseEntity = ResponseEntity.status(HttpStatus.NOT_FOUND).body(failedResult);
+        }
+        return responseEntity;
     }
 
     @Override
     public ResponseEntity<?> start() {
-        return linkParser.startParse();
+        Result result = new Result();
+        result.setResult(true);
+        ResponseEntity.status(HttpStatus.OK).body(result);
+        if (!atomicBoolean.get()){
+            result.setResult(true);
+            LinkParser linkParser = new LinkParser(sitesList,siteRepository,pageRepository,lemmaRepository,indexRepository);
+            linkParser.startParse();
+
+            return ResponseEntity.status(HttpStatus.OK).body(result);
+        }else {
+            FailedResult failedResult = new FailedResult();
+            failedResult.setResult(result);
+            failedResult.setError("indexing started");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(failedResult);
+        }
     }
 }
