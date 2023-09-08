ed3d8d9afe620d9f4334af87df07ac305fa941b6
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-09-08 13:51:56 +0500

    был добавлен метод search с условиями

--- src/main/java/searchengine/services/SearchEngineImpl.java
+++ src/main/java/searchengine/services/SearchEngineImpl.java
@@ -1,15 +1,39 @@
 package searchengine.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import searchengine.Busines.Search.Search;
+import searchengine.config.SitesList;
+import searchengine.dto.result.FailedResult;
+import searchengine.dto.result.Result;
+import searchengine.dto.search.SuccessSearchResult;
+import searchengine.services.Interface.SearchEngine;
 
 @Service
+@RequiredArgsConstructor
 public class SearchEngineImpl implements SearchEngine {
+    private final SitesList sitesList;
     @Override
-    public void findIn(String siteName, String[] word) {
-    }
-
-    @Override
-    public void findAll(String[] word) {
-
+    public ResponseEntity<?> search(String siteName, String[] word) {
+        Result result = new Result();
+        if (!word[0].isEmpty()){
+            result.setResult(true);
+            SuccessSearchResult successResult = new SuccessSearchResult();
+            successResult.setResult(result.isResult());
+            Search search = new Search(word,siteName,sitesList);
+            search.startSearch();
+            successResult.setCount(search.getCount());
+            successResult.setData(search.getFoundedData());
+            return ResponseEntity.status(HttpStatus.OK).body(successResult);
+        }
+        else {
+            result.setResult(false);
+            FailedResult failedResult = new FailedResult();
+            failedResult.setResult(result);
+            failedResult.setError("Задан неверный поисковый запрос");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(failedResult);
+        }
     }
 }
