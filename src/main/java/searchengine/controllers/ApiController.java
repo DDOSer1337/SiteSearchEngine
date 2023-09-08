7143c83bdcfbda2d2f1a810e99f6414954fa0fdf
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-09-08 13:57:46 +0500

    Добавлен ответ для search + комментарии

--- src/main/java/searchengine/controllers/ApiController.java
+++ src/main/java/searchengine/controllers/ApiController.java
@@ -3,8 +3,9 @@ package searchengine.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import searchengine.dto.statistics.StatisticsResponse;
-import searchengine.services.Indexing;
-import searchengine.services.StatisticsService;
+import searchengine.services.Interface.Indexing;
+import searchengine.services.Interface.SearchEngine;
+import searchengine.services.Interface.StatisticsService;
 
 @RestController
 @RequestMapping("/api")
@@ -12,28 +13,34 @@ public class ApiController {
 
     private final StatisticsService statisticsService;
     private final Indexing indexing;
+    private final SearchEngine searchEngine;
 
-    public ApiController(StatisticsService statisticsService,Indexing indexing) {
+    public ApiController(StatisticsService statisticsService, Indexing indexing, SearchEngine searchEngine) {
         this.statisticsService = statisticsService;
         this.indexing = indexing;
+        this.searchEngine = searchEngine;
     }
-
+    // Сделано
     @GetMapping("/statistics")
     public ResponseEntity<StatisticsResponse> statistics() {
         return ResponseEntity.ok(statisticsService.getStatistics());
     }
+    // Сделано
     @GetMapping("/startIndexing")
     public ResponseEntity<?> startIndexing(){
         return indexing.start();
     }
+    // Сделано
     @GetMapping("/stopIndexing")
     public ResponseEntity<?> stopIndexing(){
         return indexing.stop();
     }
+    // В процессе
     @GetMapping("/search")
     public ResponseEntity<?> search(@RequestParam("query") String query,@RequestParam(value = "site", required = false) String site){
-        return null;
+        return searchEngine.search(site,query.split(" "));
     }
+    //В процессе
     @PostMapping("/indexPage")
     public ResponseEntity<?> indexPage(@RequestParam("url") String url){
         return null;
