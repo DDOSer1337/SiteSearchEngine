0b952d7e69b62206bcb55436da5057c4a8f84ccd
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-09-29 23:30:07 +0500

    добавил лимит на подгруздки данных

--- src/main/java/searchengine/services/Interface/SearchEngine.java
+++ src/main/java/searchengine/services/Interface/SearchEngine.java
@@ -3,5 +3,5 @@ package searchengine.services.Interface;
 import org.springframework.http.ResponseEntity;
 
 public interface SearchEngine {
-    ResponseEntity<?> search(String siteName,String[] word);
+    ResponseEntity<?> search(String siteName,String[] word,int offset);
 }
