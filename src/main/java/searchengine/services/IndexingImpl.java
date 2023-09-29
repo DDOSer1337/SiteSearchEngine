14fa2c68201cec5521e49a7edd379836719f47d9
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-09-29 23:27:09 +0500

    добавил проверку на индексацию

--- src/main/java/searchengine/services/IndexingImpl.java
+++ src/main/java/searchengine/services/IndexingImpl.java
@@ -4,31 +4,29 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import searchengine.Busines.LinkHandling.LinkParser;
+import searchengine.Busines.LinkHandling.SiteParser;
 import searchengine.dto.result.FailedResult;
 import searchengine.dto.result.Result;
 import searchengine.dto.result.SuccessResult;
 import searchengine.services.Interface.Indexing;
 
-import java.util.concurrent.atomic.AtomicBoolean;
-
 @Service
 @RequiredArgsConstructor
 public class IndexingImpl implements Indexing {
-    public static AtomicBoolean isIndexing = new AtomicBoolean(false);
-    private final LinkParser linkParser;
+    private final SiteParser linkParser;
 
     @Override
     public ResponseEntity<?> stop() {
         Result result = new Result();
         result.setResult(isIndexing.get());
-        if (result.isResult()){
-            isIndexing.getAndSet(false);
+        if (result.isResult()) {
+            synchronized (isIndexing) {
+                isIndexing.getAndSet(false);
+            }
             SuccessResult successResult = new SuccessResult();
             successResult.setResult(result);
             return ResponseEntity.status(HttpStatus.OK).body(successResult);
-        }
-        else {
+        } else {
             FailedResult failedResult = new FailedResult();
             failedResult.setResult(result);
             failedResult.setError("indexing not started");
@@ -39,13 +37,18 @@ public class IndexingImpl implements Indexing {
     @Override
     public ResponseEntity<?> start() {
         Result result = new Result();
-        if (!isIndexing.get()){
-            result.setResult(true);
+        if (!isIndexing.get()) {
+            System.out.println("not indexed " + isIndexing.get());
+            synchronized (isIndexing) {
+                isIndexing.getAndSet(true);
+            }
+            result.setResult(isIndexing.get());
             new Thread(linkParser::startParse).start();
             SuccessResult successResult = new SuccessResult();
             successResult.setResult(result);
             return ResponseEntity.status(HttpStatus.OK).body(successResult);
-        }else {
+        } else {
+            System.out.println("indexed " + isIndexing.get());
             result.setResult(false);
             FailedResult failedResult = new FailedResult();
             failedResult.setResult(result);
