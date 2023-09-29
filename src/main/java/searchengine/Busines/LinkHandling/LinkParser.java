359c4391e86f63f25186956ad5c40875a0876218
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-09-29 23:16:23 +0500

    переименован метод

--- src/main/java/searchengine/Busines/LinkHandling/LinkParser.java
+++ src/main/java/searchengine/Busines/LinkHandling/LinkParser.java
@@ -1,12 +1,17 @@
 package searchengine.Busines.LinkHandling;
 
-import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
+import org.jsoup.Connection;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Element;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import searchengine.config.SitesList;
-import searchengine.dto.result.Result;
+import searchengine.Busines.LemmaCreator;
+import searchengine.model.Enum.SiteStatus;
+import searchengine.model.Index;
+import searchengine.model.Lemma;
+import searchengine.model.Page;
 import searchengine.model.Site;
 import searchengine.repositories.IndexRepository;
 import searchengine.repositories.LemmaRepository;
@@ -14,17 +19,17 @@ import searchengine.repositories.PageRepository;
 import searchengine.repositories.SiteRepository;
 import searchengine.services.IndexingImpl;
 
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.io.IOException;
+import java.util.*;
+import java.util.concurrent.RecursiveAction;
 
-@Service @Getter @Setter
 @RequiredArgsConstructor
-public class LinkParser {
-    private String domain;
-    private Set<String> verifiedLinks = Collections.synchronizedSet(new HashSet<>());
-    private final SitesList sitesList;
+@Setter
+@Service
+public class LinkParser extends RecursiveAction {
+    private String domain, currentLink;
+    private Set<String> verifiedLinks;
+    private Site site;
     @Autowired
     private final SiteRepository siteRepository;
     @Autowired
@@ -33,32 +38,101 @@ public class LinkParser {
     private final LemmaRepository lemmaRepository;
     @Autowired
     private final IndexRepository indexRepository;
-    private final LinkCrawler linkCrawler;
 
 
-    public void startParse() {
-        List<searchengine.config.Site> listSites = sitesList.getSites();
-        for (searchengine.config.Site siteFromList : listSites) {
-            String url = siteFromList.getUrl();
-            if (!IndexingImpl.isIndexing.get() && isURL(url)) {
-                IndexingImpl.isIndexing.getAndSet(true);
-                domain = url.split("/")[2];
-                Site site = new Site(url, domain);
-                if (siteRepository.existsByName(site.getName())) {
-                    siteRepository.deleteByName(site.getName());
+    @Override
+    protected void compute() {
+        if (IndexingImpl.isIndexing.get() && !verifiedLinks.contains(currentLink)) {
+            linkChecking();
+        }
+    }
+
+    private void linkChecking() {
+        Connection connection = Jsoup.connect(currentLink)
+                .userAgent("Mozilla/5.0 (Windows; U; WindowsNT " +
+                        "5.1; en-US; rv1.8.1.6) Gecko/20070725 Firefox/2.0.0.6")
+                .referrer("http://www.google.com");
+        try {
+            List<Element> links = connection.get().select("a[href]");
+            for (Element link : links) {
+                if (!verifiedLinks.contains(link.baseUri())) {
+                    String newLink = link.attr("abs:href");
+                    recursiveActionFork(newLink, connection);
                 }
-                siteRepository.save(site);
-                linkCrawler.setDomain(domain);
-                linkCrawler.setCurrentLink(siteFromList.getUrl());
-                linkCrawler.setSite(site);
-                linkCrawler.setVerifiedLinks(verifiedLinks);
-                linkCrawler.compute();
+            }
+            verifiedLinks.add(currentLink);
+        } catch (IOException e) {
+            exceptionSite(site);
+            e.printStackTrace();
+        }
+    }
+
+    private void recursiveActionFork(String newLink, Connection connection) throws IOException {
+        Optional<Site> site = Optional.ofNullable(siteRepository.findByName(domain));
+        if (site.isPresent()) {
+            Page page = new Page(newLink, connection.get(), domain, site.get(), connection.execute().statusCode());
+            if (IndexingImpl.isIndexing.get()) {
+                if (!pageExist(page) && page.getPath()!=null) {
+                    pageRepository.save(page);
+                    page = pageRepository.findByPathAndSiteId_Name(page.getPath(),site.get().getName());
+                    List<Lemma> list = getLemmas(connection, site.get());
+                    for (Lemma lemma : list) {
+                        if (IndexingImpl.isIndexing.get() && lemma != null) {
+                            indexCreator(page, lemma);
+                            forking(newLink);
+                        } else {
+                            endSiteSearch();
+                        }
+                    }
+                } else {
+                    endSiteSearch();
+                }
             }
         }
     }
 
-    private boolean isURL(String url) {
-        return url != null && (url.matches("^(https?)://(www.)?[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"));
+    private boolean pageExist(Page page) {
+        if (domain.startsWith("www.")) {
+            return pageRepository.existsByPathAndSiteId_name(page.getPath(), domain.substring(4));
+        } else {
+            return pageRepository.existsByPathAndSiteId_name(page.getPath(), domain);
+        }
     }
 
+    private List<Lemma> getLemmas(Connection connection, Site site) throws IOException {
+        LemmaCreator lemmaCreator = new LemmaCreator(lemmaRepository, connection.get(), site,true);
+        lemmaCreator.createLemmas();
+        return lemmaCreator.getListLemmas();
+    }
+
+    private void indexCreator(Page page, Lemma lemma) {
+        Index index = new Index(page, lemma);
+        if (!indexRepository.existsByLemmaIdAndPageId(lemma, page)) {
+            indexRepository.save(index);
+        } else {
+            indexRepository.upRank(page.getId(), lemma.getId());
+        }
+    }
+
+    private void forking(String newLink) {
+        if (IndexingImpl.isIndexing.get()) {
+            LinkParser linkCrawler = new LinkParser(siteRepository, pageRepository, lemmaRepository, indexRepository);
+            linkCrawler.setCurrentLink(newLink);
+            linkCrawler.setDomain(domain);
+            linkCrawler.setCurrentLink(newLink);
+            linkCrawler.setSite(site);
+            linkCrawler.setVerifiedLinks(verifiedLinks);
+            linkCrawler.fork();
+        } else {
+            endSiteSearch();
+        }
+    }
+
+    private void exceptionSite(Site site) {
+        siteRepository.UpdateErrorByName(site.getName(), SiteStatus.FAILED.toString());
+    }
+
+    private void endSiteSearch() {
+        siteRepository.UpdateErrorByName(site.getName(), SiteStatus.INDEXED.toString());
+    }
 }
