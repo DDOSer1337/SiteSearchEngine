553ad3ada197974c731c739be2f79650e81b4f55
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-09-29 23:16:50 +0500

    переименован метод из LinkParser

---
+++ src/main/java/searchengine/Busines/LinkHandling/SiteParser.java
@@ -0,0 +1,65 @@
+package searchengine.Busines.LinkHandling;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import searchengine.config.SitesList;
+import searchengine.model.Site;
+import searchengine.repositories.IndexRepository;
+import searchengine.repositories.LemmaRepository;
+import searchengine.repositories.PageRepository;
+import searchengine.repositories.SiteRepository;
+import searchengine.services.IndexingImpl;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Service @Getter @Setter
+@RequiredArgsConstructor
+public class SiteParser {
+    private String domain;
+    private Set<String> verifiedLinks = Collections.synchronizedSet(new HashSet<>());
+    private final SitesList sitesList;
+    @Autowired
+    private final SiteRepository siteRepository;
+    @Autowired
+    private final PageRepository pageRepository;
+    @Autowired
+    private final LemmaRepository lemmaRepository;
+    @Autowired
+    private final IndexRepository indexRepository;
+    private final LinkParser linkCrawler;
+
+
+    public void startParse() {
+        List<searchengine.config.Site> listSites = sitesList.getSites();
+        for (searchengine.config.Site siteFromList : listSites) {
+            String url = siteFromList.getUrl();
+            if (IndexingImpl.isIndexing.get() && isURL(url)) {
+                domain = url.split("/")[2];
+                if (domain.startsWith("www.")) {
+                    domain = domain.substring(4);
+                }
+                Site site = new Site(url, domain);
+                if (siteRepository.existsByName(site.getName())) {
+                    siteRepository.deleteByName(site.getName());
+                }
+                siteRepository.save(site);
+                linkCrawler.setDomain(domain);
+                linkCrawler.setCurrentLink(siteFromList.getUrl());
+                linkCrawler.setSite(site);
+                linkCrawler.setVerifiedLinks(verifiedLinks);
+                linkCrawler.compute();
+            }
+        }
+    }
+
+    private boolean isURL(String url) {
+        return url != null && (url.matches("^(https?)://(www.)?[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"));
+    }
+
+}
