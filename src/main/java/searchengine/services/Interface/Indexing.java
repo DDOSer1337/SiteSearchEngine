ba9f5d7cb0272cf1c6b9f36eaed06fb87e53cd68
DDOSer1337 <59763808+DDOSer1337@users.noreply.github.com>
2023-09-29 23:26:36 +0500

    добавил проверку на индесацию

--- src/main/java/searchengine/services/Interface/Indexing.java
+++ src/main/java/searchengine/services/Interface/Indexing.java
@@ -2,8 +2,11 @@ package searchengine.services.Interface;
 
 import org.springframework.http.ResponseEntity;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 
 public interface Indexing {
+    AtomicBoolean isIndexing = new AtomicBoolean(false);
     ResponseEntity<?> stop();
     ResponseEntity<?> start();
 }
